244209c0a5166babae125183ffc0b6ca759d6e40
Mohammad <mohammad@Mohammads-Mac-mini-2.local>
2025-03-25 03:07:24 -0600

    updated donation amount 39297

--- app/components/EnhancedProgressBar.tsx
+++ app/components/EnhancedProgressBar.tsx
@@ -49,7 +49,7 @@ interface ProgressBarProps {
 }
 
 const EnhancedProgressBar: React.FC<ProgressBarProps> = ({ 
-  currentAmount = 37477, 
+  currentAmount = 39297, 
   goalAmount = 200000, 
   currency = '$',
   title = 'Fundraising Progress',
@@ -58,7 +58,7 @@ const EnhancedProgressBar: React.FC<ProgressBarProps> = ({
   height = 'medium',
   showMilestones = true,
 //   recentDonors = [],
-  donations = 128
+  donations = 139
 }) => {
   const [isHovered, setIsHovered] = useState(false);
   
