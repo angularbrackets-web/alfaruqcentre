41d501b83937981c4a9f9d7290d3b98afb9e72ba
Mohammad <mohammad@Mohammads-Mac-mini-2.local>
2025-03-25 03:05:18 -0600

    khatim al quran poster slide added

--- app/components/HeroSectionTable.tsx
+++ app/components/HeroSectionTable.tsx
@@ -129,7 +129,19 @@ const ContentBlock: React.FC<{
 const HeroSectionTable = () => {
   const contentBlocks = useMemo<ContentBlockType[]>(
     () => [
-      
+      {
+        type: "image",
+        pretitle: "",
+        title: "",
+        introduction: null,
+        description: (
+          <div className="mt-32">
+            <Image src="/AlFaruq.KhatimalQuran.jpeg" alt="" className="w-full" width={400} height={400} />
+          </div>
+        ), // or you can pass JSX here if needed
+        imageSrc: "",
+        duration: 15000,
+      },
       {
         type: "image",
         pretitle: "",
